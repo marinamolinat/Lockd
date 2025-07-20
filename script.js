277cec55732cb519fca753e63f8d6f358a471c41
Marina M <marinamolina7877@gmail.com>
2025-07-20 18:06:05 -0500

    made a working countDown

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
-let timeText = document.querySelector("#time");
+const timeText = document.querySelector("#time");
 
 let windowElements = document.getElementsByClassName("window");
-let closeButtons = document.getElementsByClassName("close");
+const countDownButton = document.getElementById("countdownButton");
+const display = document.getElementById("countdownDisplay");
+let timeValue = null;
 
 
 
-
+//time and countDown
 function updateTime() {
+    //current time
         let currentTime = new Date().toLocaleString();
         timeText.innerHTML = currentTime;
-    }
-    setInterval(updateTime, 1000);
 
-updateTime();
+
+        //count down
+        if(!timeValue){
+            display.innerText = `CountDown`;
+
+
+        }
+        else {
+             //Unix time babyy
+        currentTime = new Date().getTime();
+        let countDownDate = new Date(timeValue).getTime();
+        let distance = countDownDate-currentTime;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        display.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+
+        }
+    }
+
+setInterval(updateTime, 1000);
+
+
+
+countDownButton.addEventListener("click", function() {
+   
+
+    const input = document.getElementById("inputDate");
+
+    timeValue = input.value;
+
+
+    
+});
+
+
 
 
 
@@ -29,6 +69,29 @@ document.querySelectorAll('.close').forEach(button => {
     }
   });
 });
+
+//open button
+document.querySelectorAll('.open').forEach(button => {
+  button.addEventListener('click', function () {
+    for (let el of windowElements) {
+        if (button.id === el.id + "Button")
+        {
+            el.style.display = 'flex';
+        }
+ 
+}
+
+   
+  });
+});
+
+
+
+
+
+
+
+
 
 for (let el of windowElements) {
     dragElement(el);
